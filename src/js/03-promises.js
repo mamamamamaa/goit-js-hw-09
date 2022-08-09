832dbab9419a132ad62426d84d8fbad746baefe0
Max Denishchuk <maks.denishchuk@gmail.com>
2022-08-09 21:44:46 +0300

    //

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,18 +18,17 @@ function createPromise(position, delay) {
 
 formRef.addEventListener('submit', e => {
   e.preventDefault();
-  const delay = Number(e.currentTarget.delay.value);
+  let delay = Number(e.currentTarget.delay.value);
   const step = Number(e.currentTarget.step.value);
   const amount = Number(e.currentTarget.amount.value);
-  let k = delay;
   for (let i = 0; i < amount; i++) {
-    createPromise(i + 1, k)
+    createPromise(i + 1, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    k += step;
+    delay += step;
   }
 });
